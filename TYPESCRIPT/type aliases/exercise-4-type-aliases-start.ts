b9e022e45af7c3541f90897ba032cd4875bc8b35
Okkes <okkesakkurt46@gmail.com>
2023-10-11 17:01:16 +0300

    Any, unknown and never types completed

---
+++ TYPESCRIPT/type aliases/exercise-4-type-aliases-start.ts
@@ -0,0 +1,28 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Create a `Country` type alias for the `string` type.
+// Change the type for each variable to use the `Country` type.
+
+const Country = (varbl: any): any => {
+    console.log(varbl);
+};
+
+const country1: string = "Nigeria";
+
+const country2: string = "Italy";
+
+const country3: string = "China";
+
+
+const country = Country(country1);
+/*
+const country = Country(country2);
+const country = Country(country3);
+*/
+
+
+// ----
+
+export {};
